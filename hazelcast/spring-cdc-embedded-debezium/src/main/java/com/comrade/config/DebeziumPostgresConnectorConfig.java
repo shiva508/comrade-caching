a5961450b593d885fe34882a93948744e4f83ca6
shiva508 <dasarishiva1@gmail.com>
2023-11-22 10:06:57 +0530

    Debezium Postgres data sync with spring boot(refactor)

--- hazelcast/spring-cdc-embedded-debezium/src/main/java/com/comrade/config/DebeziumPostgresConnectorConfig.java
+++ hazelcast/spring-cdc-embedded-debezium/src/main/java/com/comrade/config/DebeziumPostgresConnectorConfig.java
@@ -12,13 +12,28 @@ public class DebeziumPostgresConnectorConfig {
     @Autowired
     private CustomerConfig customerConfig;
 
-    @Bean
+    @Bean(name = "customerConnector")
     public io.debezium.config.Configuration customerConnector() throws IOException {
         System.out.println(customerConfig);
         File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
         File dbHistoryTempFile = File.createTempFile("dbhistory_", ".dat");
         return io.debezium.config.Configuration.create()
-                /*.with("name", "customer-postgres-connector")
+                //WORKING
+                .with("name", "engine")
+                .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore").with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
+                .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
+                .with("offset.flush.interval.ms", "60000")
+                .with("database.hostname", customerConfig.getHost())
+                .with("database.port", customerConfig.getPort())
+                .with("database.user", customerConfig.getUsername())
+                .with("database.password", customerConfig.getPassword()).with("database.dbname", customerConfig.getDatabase())
+                .with("database.server.id", "85744")
+                .with("topic.prefix", "my-app-connector")
+                .with("schema.history.internal", "io.debezium.storage.file.history.FileSchemaHistory")
+                .with("schema.history.internal.file.filename", dbHistoryTempFile.getAbsolutePath()).build();
+        //WORKING
+        //NEED TO TRY WITH BELLOW
+        /*.with("name", "customer-postgres-connector")
                 .with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
                 .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
                 .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
@@ -41,18 +56,5 @@ public class DebeziumPostgresConnectorConfig {
                 .with("schema.history.internal.file.filename", dbHistoryTempFile.getAbsolutePath())
                 .with("snapshot.delay.ms", "100")
                 .with("errors.log.include.messages", "true")*/
-        .with("name", "engine")
-        .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore").with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
-        .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
-        .with("offset.flush.interval.ms", "60000")
-        /* begin connector properties */
-        .with("database.hostname", customerConfig.getHost())
-        .with("database.port", customerConfig.getPort())
-        .with("database.user", customerConfig.getUsername())
-        .with("database.password", customerConfig.getPassword()).with("database.dbname", customerConfig.getDatabase())
-        .with("database.server.id", "85744")
-        .with("topic.prefix", "my-app-connector")
-        .with("schema.history.internal", "io.debezium.storage.file.history.FileSchemaHistory")
-        .with("schema.history.internal.file.filename", dbHistoryTempFile.getAbsolutePath()).build();
     }
 }
